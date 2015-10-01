046706e808beedaba511969cf1c7aae01e8008da
psyflo <florian.bruehlmann@gmail.com>
2015-10-01 10:36:02 +0200

    Update EFSlabels.R

--- R/EFSlabels.R
+++ R/EFSlabels.R
@@ -1,7 +1,3 @@
-# Hello, world!  This is an example function named 'hello' which prints 'Hello, world!'.  You can learn more
-# about package authoring with RStudio at: http://r-pkgs.had.co.nz/ Some useful keyboard shortcuts for package
-# authoring: Build and Reload Package: 'Cmd + Shift + B' Check Package: 'Cmd + Shift + E' Test Package: 'Cmd +
-# Shift + T'
 
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage("Welcome to my EFSlabels v0.1\nCall the function with EFSlabels()")
